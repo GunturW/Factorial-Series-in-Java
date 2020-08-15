3a975ca9adce3ebd00f7111cd77621fbc5282f14
Guntur Wicaksono <gunturw79@gmail.com>
2020-08-15 14:10:01 +0700

    Factorial

---
+++ src/factorial/Factorial.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorial;
+
+/**
+ *
+ * @author 247
+ */
+import java.util.Scanner;
+public class Factorial {
+    static int factorial(int n){
+        if (n == 0){
+            return 1;
+        }else{
+            return(n * factorial(n-1));
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner reader = new Scanner(System.in);
+        System.err.print("Masukkan angka yang ingin difactorialkan: ");
+        int number = reader.nextInt();
+        int numfactorial = factorial(number);
+        System.err.printf("Hasil factorial dari %d adalah: %d ", number, numfactorial);
+    }
+    
+}
